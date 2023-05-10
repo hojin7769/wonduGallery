fb129a3365265409bd7bfc1afe66e4fe50dfb0c9
박호진 <hjpark@goodcen.com>
2023-05-10 09:20:51 +0900

    feature: login 페이지 주입
    로그인 페이지 생성 및 elements UI 적용

--- src/main/java/com/example/wondugallery/web/FileController.java
+++ src/main/java/com/example/wondugallery/web/FileController.java
@@ -1,4 +1,14 @@
 package com.example.wondugallery.web;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api")
 public class FileController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test(){
+        return new ResponseEntity<>("test",null);
+    }
 }
