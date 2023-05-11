2d16d1ae3fcd83d90b67e3d722839af85724e270
박호진 <hjpark@goodcen.com>
2023-05-11 15:15:22 +0900

    feature: login 페이지 주입
    로그인 페이지 생성 및 elements UI 적용

--- src/test/java/com/example/wondugallery/jasypt/JasyptTest.java
+++ src/test/java/com/example/wondugallery/jasypt/JasyptTest.java
@@ -6,16 +6,6 @@ public class JasyptTest {
 
     public static void main(String[] args) {
 
-        String password = "jdbc:mysql://141.164.53.210:3306/test";
 
-        StandardPBEStringEncryptor jasypt = new StandardPBEStringEncryptor();
-        jasypt.setPassword("MyPrivateKey");
-        jasypt.setAlgorithm("PBEWITHMD5ANDDES");
-
-        String encryptedText = jasypt.encrypt(password);
-        String decryptedText = jasypt.decrypt(encryptedText);
-
-        System.out.println("encryptedText = " + encryptedText);
-        System.out.println("decryptedText = " + decryptedText);
     }
 }
